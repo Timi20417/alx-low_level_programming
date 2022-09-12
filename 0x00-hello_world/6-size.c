50391dd680c70dc91fd38d8e89135a08c39c3803
Timi20417 <timmyjohn006@gmail.com>
2022-09-12 03:25:40 -0700

    Timi

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - prints the size of various types
+*
+*Description: using the main function 
+this project prints "Programming is like building a multilingual puzzle 
+*Return: 0 
+*/ 
+int main(void)
+{
+        char c;
+        int i;
+        float f;
+
+        printf("Size of a char: %ld byte(s)\n",sizeof(c))
+        printf("Size of an int:%ld byte(s)\n",sizeof(i));
+        return (0);
+}
