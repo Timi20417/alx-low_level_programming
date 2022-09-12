7699156f992f820953d54b9b7ccab916ba4e5b48
Timi20417 <timmyjohn006@gmail.com>
2022-09-12 03:47:00 -0700

    Timi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - print the string in the put function 
+*main - print the string in the put function
 *
-*Description: using the main function 
-*this program prints "Programming is like building a multilingua* Return: 0
+*Description: using the main function
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
